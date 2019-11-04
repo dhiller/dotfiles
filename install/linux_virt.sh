4592bcc3155e238027f0a5c7c7fd2572948f8e54
Daniel Hiller <daniel.hiller.1972@gmail.com>
2019-11-04 09:58:21 +0100

    Add kubernetes-client and krew

--- install/linux_virt.sh
+++ install/linux_virt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo dnf install -y make docker
+sudo dnf install -y make docker kubernetes-client
 
 # allow usage w/o root
 sudo groupadd docker
@@ -9,6 +9,16 @@ sudo gpasswd -a $USER docker
 # enable docker daemon on system start
 sudo systemctl enable docker 
 sudo systemctl start docker 
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.3.1/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  ./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" install \
+    --manifest=krew.yaml --archive=krew.tar.gz
+)
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 echo "WARNING: this will terminate current gnome session! Continue? (y/N)"
 read answer
